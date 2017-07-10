dffbcf8539dd4e5680570cd2ec7af59d27f5f7a0
SORA0920 <sora02230112@gmail.com>
2017-07-10 15:43:26 +0900

    test

--- higher.js
+++ higher.js
@@ -1,9 +1,22 @@
-function new_func (func){
-    console.log('new_func');
-    func();
+// function new_func (func){
+//     console.log('new_func');
+//     func();
+// }
+
+// function func1(){
+//     console.log("func1");
+// }
+// new_func(func1);
+
+function arrayWalk(data, f){
+    for(var key in data){
+        f(data[key], key);
+    }
 }
 
-function func1(){
-    console.log("func1");
+function showElement(value, key){
+    console.log(key + ':' + value);
 }
-new_func(func1);
+var ary = [1, 2, 4, 8, 16];
+// arrayWalk(ary, showElement);
+ary.forEach(showElement);
